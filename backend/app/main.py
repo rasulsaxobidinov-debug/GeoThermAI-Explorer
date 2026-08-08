ea4205f2006a5af33b7d10d3bb94f4862e7e630d
Rasul Saxobidinov <rasul.saxobidinov@gmail.com>
2026-08-08 11:52:22 +0500

    Add backend/app/main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,16 +11,13 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-    ],
+    allow_origins=['http://localhost:3000', 'http://127.0.0.1:3000'],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["Content-Type"],
 )
 
-AI_ENGINE_URL = "http://ai-engine:8001/predict"
+AI_ENGINE_URL = 'http://ai-engine:8001/predict'
 
 
 @app.get("/")
